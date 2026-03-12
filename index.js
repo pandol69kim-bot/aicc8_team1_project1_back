c5a3449c7c67948a9e6bcff6d7b906eda2467915
pandol <pandol69.kim@gmail.com>
2026-03-12 15:09:31 +0900

    .env change  2222

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ import recommendRoutes from './src/routes/recommend.routes.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './database/swagger.js';
 
-// test - .env change
+// test - .env change  2222
 const app = express();
 
 // Set uploads directory as static route
